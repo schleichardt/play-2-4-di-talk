ce1e3d41db1e6696c355615c8ff82591d5b1c43a
schleichardt <michael.schleichardt@commercetools.de>
2015-05-20 18:13:02 +0200

    Use ProdMongoClient in ProdModule.

--- play-24/app/modules/ProdModule.java
+++ play-24/app/modules/ProdModule.java
@@ -1,7 +1,7 @@
 package modules;
 
 import com.mongodb.MongoClient;
-import mongo.DevMongoClientProvider;
+import mongo.ProdMongoClientProvider;
 import play.api.Configuration;
 import play.api.Environment;
 import play.api.inject.Binding;
@@ -11,6 +11,6 @@ import scala.collection.Seq;
 public class ProdModule extends Module {
     @Override
     public Seq<Binding<?>> bindings(final Environment environment, final Configuration configuration) {
-        return seq(bind(MongoClient.class).toProvider(DevMongoClientProvider.class));
+        return seq(bind(MongoClient.class).toProvider(ProdMongoClientProvider.class));
     }
 }
